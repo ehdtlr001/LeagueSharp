b65bb81fcdbdc4b885231cc2efec74073e6a0447
ehdtlr001 <marketer000000@gmail.com>
2016-01-07 19:32:55 +0900

    SN Brand

--- Brand/Brand/Program.cs
+++ Brand/Brand/Program.cs
@@ -212,13 +212,13 @@ namespace Brand
                 {
                     if (GetDamage(target) > target.Health)
                         _Ignite.CastOnUnit(target);
-                    if (Player.Distance(target) > 300 && QcollC)
+                    if (Player.Distance(target) > 300)
                     {
                         Q.CastIfHitchanceEquals(target, Hitchance("Use_Q"), true);
                         E.CastOnUnit(target);
                         W.CastIfHitchanceEquals(target, Hitchance("Use_W"), true);
                     }
-                    else if (QcollC)
+                    else
                     {                        
                         E.CastOnUnit(target);
                         Q.CastIfHitchanceEquals(target, Hitchance("Use_Q"), true);
