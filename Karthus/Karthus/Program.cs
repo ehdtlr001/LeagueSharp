102c0c41777e4c3f50adee7df56a6099d43b3922
ehdtlr001 <marketer000000@gmail.com>
2016-01-10 01:26:25 +0900

    SN Karthus (Based on Beaving's KarthusSharp)
    SN Karthus (Based on Beaving's KarthusSharp)

--- Karthus/Karthus/Program.cs
+++ Karthus/Karthus/Program.cs
@@ -145,7 +145,7 @@ namespace Karthus
 
                 var time = Utils.TickCount;
 
-                foreach (TI target in Program.Check.TI.Where(x => x.Player.IsValid && !x.Player.IsDead && x.Player.IsEnemy && (Program.Check.recalltc(x) || (x.Player.IsVisible && Utility.IsValidTarget(x.Player))) && Player.GetSpellDamage(x.Player, SpellSlot.R) >= Program.Check.GetTargetHealth(x, (int)(R.Delay * 1000f))))
+                foreach (TI target in Program.Check.TI.Where(x => x.Player.IsValid && !x.Player.IsDead && x.Player.IsEnemy && (Program.Check.recalltc(x) /*|| (x.Player.IsVisible && Utility.IsValidTarget(x.Player))*/) && Player.GetSpellDamage(x.Player, SpellSlot.R) >= Program.Check.GetTargetHealth(x, (int)(R.Delay * 1000f))))
                 {
                     killable += target.Player.ChampionName + " ";
                 }
